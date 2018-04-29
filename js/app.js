e23b1a336fee076ec333e58718c5d8ad31f02fbe
simonhfrost <simonhfrost@gmail.com>
2018-04-29 21:11:25 +0200

    Use path class with bezier

--- js/app.js
+++ js/app.js
@@ -8,58 +8,19 @@ const FastSimplexNoise = require('fast-simplex-noise')
 const output = initialize()
 const scene = output.scene
 
-const drawLine = (a1, a2, b1, b2) => {
-  var curve = new THREE.LineCurve(
-    new THREE.Vector2(a1, a2),
-    new THREE.Vector2(b1, b2)
-  );
+var path = new THREE.Path();
 
-  // overly complicated way to create a line, but looks like it's the best way?
-  var points = curve.getPoints( 10 )
-  var geometry = new THREE.BufferGeometry().setFromPoints( points )
-  var material = new THREE.LineBasicMaterial({
-    color : 0xffffff,
-    linewidth: 50 // doesn't do anything?
-  })
-  var curveObject = new THREE.Line( geometry, material )
+path.lineTo( 0, 8 );
+path.quadraticCurveTo( 0, 10, 2, 10 );
+path.lineTo( 10, 10 );
 
-  scene.add(curveObject)
-}
+var points = path.getPoints();
 
-const NUM_COLUMNS = 10
-const NUM_ROWS = 15
+var geometry = new THREE.BufferGeometry().setFromPoints( points );
+var material = new THREE.LineBasicMaterial( { color: 0xffffff } );
 
-// Generate columns
-for (let i = 0; i < NUM_COLUMNS; i++) {
-  let pos = (i - 4.5) * 10
-  drawLine(pos, -50, pos, 50)
-}
-
-let totals = []
-totals.length = NUM_COLUMNS
-totals.fill(-50)
-
-let noiseGens = []
-noiseGens.length = NUM_COLUMNS
-noiseGens.fill(new FastSimplexNoise.default({ frequency: 1, max: 10, min: 0, octaves: 8 }))
-
-for (let i = 0; i < NUM_ROWS; i++) {
-  for (let j = 0; j < NUM_COLUMNS; j++) {
-
-    // use perlin noise instead of full random so more organic curves are created
-    const noiseGen = noiseGens[j]
-    totals[j] += noiseGen.scaled([j, 0])
-
-    // we're about to draw from the current point to the previous one,
-    // this won't work for the first line :)
-    if (j === 0) {
-      continue;
-    }
-
-    let pos = (j - 4.5) * 10
-    drawLine(pos - 10, totals[j - 1], pos, totals[j])
-  }
-}
+var line = new THREE.Line( geometry, material );
+scene.add( line );
 
 scene.add(createAmbientLight())
 scene.add(createDirectionalLight())
