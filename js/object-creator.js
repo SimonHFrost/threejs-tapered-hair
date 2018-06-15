064fd1db363c46893107e0b00857fea15885c416
simonhfrost <simonhfrost@gmail.com>
2018-06-15 15:41:17 +0200

    Remove spaceship code

--- js/object-creator.js
+++ js/object-creator.js
@@ -10,24 +10,6 @@ function createDirectionalLight () {
   return directionalLight
 }
 
-function createSpaceship (geometry) {
-  const material = new THREE.MeshLambertMaterial({
-    color: '#ed8989',
-    flatShading: true
-  })
-
-  const spaceship = new THREE.Mesh(
-    geometry,
-    material
-  )
-
-  spaceship.scale.x = 0.1
-  spaceship.scale.y = 0.1
-  spaceship.scale.z = 0.1
-
-  return spaceship
-}
-
 function createCube () {
   const geometry = new THREE.BoxBufferGeometry( 1, 1, 1 )
   const material = new THREE.MeshLambertMaterial( { color: 0xED8989, flatShading: true } )
@@ -38,6 +20,5 @@ function createCube () {
 module.exports = {
   createAmbientLight,
   createDirectionalLight,
-  createSpaceship,
   createCube
 }
