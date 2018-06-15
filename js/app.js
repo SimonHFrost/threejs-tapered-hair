66333e4b8ab0ec2749669dea727a1b9e22ca7206
simonhfrost <simonhfrost@gmail.com>
2018-06-15 16:27:40 +0200

    Put line drawing in functions

--- js/app.js
+++ js/app.js
@@ -11,9 +11,29 @@ const createCube = require('./object-creator.js').createCube
 var path = new THREE.Path()
 path.moveTo(-50, -50)
 
+function drawPoint(x1, y1, x2, y2, x, y) {
+  path.bezierCurveTo(x1, y1, x2, y2, x, y)
+  scene.add(createCube(x1, y1, 'green'))
+  scene.add(createCube(x2, y2, 'red'))
+  scene.add(createCube(x, y, 'blue'))
+}
+
+function drawResult() {
+  var points = path.getPoints();
+
+  var geometry = new THREE.BufferGeometry().setFromPoints( points );
+  var material = new THREE.LineBasicMaterial( { color: 0xffffff } );
+
+  var line = new THREE.Line( geometry, material );
+  scene.add( line );
+
+  scene.add(createAmbientLight())
+  scene.add(createDirectionalLight())
+}
+
 for (let i = 0; i < 2; i++) {
-  let locationX = Math.random() * 50
-  let locationY = Math.random() * 50
+  let locationX = i * 50
+  let locationY = i * 50
 
   let firstAnchorX = Math.random() * 50
   let firstAnchorY = Math.random() * 50
@@ -21,21 +41,7 @@ for (let i = 0; i < 2; i++) {
   let secondAnchorX = Math.random() * 50
   let secondAnchorY = Math.random() * 50
 
-  path.bezierCurveTo( firstAnchorX, firstAnchorY, secondAnchorX, secondAnchorY, locationX, locationY )
-
-  // DIAGNOSTICS
-  scene.add(createCube(firstAnchorX, firstAnchorY, 'green'))
-  scene.add(createCube(secondAnchorX, secondAnchorY, 'red'))
-  scene.add(createCube(locationX, locationY, 'blue'))
+  drawPoint( firstAnchorX, firstAnchorY, secondAnchorX, secondAnchorY, locationX, locationY )
 }
 
-var points = path.getPoints();
-
-var geometry = new THREE.BufferGeometry().setFromPoints( points );
-var material = new THREE.LineBasicMaterial( { color: 0xffffff } );
-
-var line = new THREE.Line( geometry, material );
-scene.add( line );
-
-scene.add(createAmbientLight())
-scene.add(createDirectionalLight())
+drawResult()
