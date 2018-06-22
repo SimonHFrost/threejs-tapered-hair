6e91f9cd42193446e0646657965b8801e7db1398
simonhfrost <simonhfrost@gmail.com>
2018-06-22 17:20:44 +0200

    Add dat.gui

--- js/app.js
+++ js/app.js
@@ -7,10 +7,21 @@ const createDirectionalLight = require('./object-creator.js').createDirectionalL
 const createCurve = require('./object-creator.js').createCurve
 const createLine = require('./object-creator.js').createLine
 
+const dat = require('dat.gui')
+
 const output = initialize()
 const scene = output.scene
 scene.add(createAmbientLight())
 scene.add(createDirectionalLight())
+
+var gui = new dat.GUI()
+var controls = {
+  range: 200,
+  generate: function() { console.log('ok') }
+}
+
+gui.add(controls, 'range', 0, 1000)
+gui.add(controls, 'generate')
 
 function getRandomNearby(value, length) {
   return value - (length / 2) + Math.random() * length
@@ -18,8 +29,8 @@ function getRandomNearby(value, length) {
 
 var path = new THREE.Path()
 
-let previousAnchorX = Math.random() * 200
-let previousAnchorY = Math.random() * 200
+let previousAnchorX = Math.random() * controls.range
+let previousAnchorY = Math.random() * controls.range
 
 let connectingX = 0
 let connectingY = 0
@@ -31,8 +42,8 @@ for (let i = 0; i < 5; i++) {
   previousAnchorX = connectingX + (connectingX - anchor2X)
   previousAnchorY = connectingY + (connectingY - anchor2Y)
 
-  let nextConnectingX = Math.random() * 200
-  let nextConnectingY = Math.random() * 200
+  let nextConnectingX = Math.random() * controls.range
+  let nextConnectingY = Math.random() * controls.range
 
   let nextAnchor1X = getRandomNearby(nextConnectingX, 100)
   let nextAnchor1Y = getRandomNearby(nextConnectingY, 100)
