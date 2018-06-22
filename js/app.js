e012f067fee2751185426ab7e9e184466b6085fa
simonhfrost <simonhfrost@gmail.com>
2018-06-22 17:42:28 +0200

    Hook up gui to generation

--- js/app.js
+++ js/app.js
@@ -9,23 +9,25 @@ const createLine = require('./object-creator.js').createLine
 
 const dat = require('dat.gui')
 
-const output = initialize()
-const scene = output.scene
-scene.add(createAmbientLight())
-scene.add(createDirectionalLight())
-
 var gui = new dat.GUI()
 var controls = {
-  range: 200,
-  generate: function() { console.log('ok') }
+  range: 500,
+  anchorRange: 200,
+  generate: () => { console.log('generate') }
 }
 
 gui.add(controls, 'range', 0, 1000)
+gui.add(controls, 'anchorRange', 0, 1000)
 gui.add(controls, 'generate')
 
 function getRandomNearby(value, length) {
   return value - (length / 2) + Math.random() * length
 }
+
+const output = initialize()
+const scene = output.scene
+scene.add(createAmbientLight())
+scene.add(createDirectionalLight())
 
 var path = new THREE.Path()
 
@@ -35,8 +37,8 @@ let previousAnchorY = Math.random() * controls.range
 let connectingX = 0
 let connectingY = 0
 
-let anchor2X = getRandomNearby(connectingX, 100)
-let anchor2Y = getRandomNearby(connectingY, 100)
+let anchor2X = getRandomNearby(connectingX, controls.anchorRange)
+let anchor2Y = getRandomNearby(connectingY, controls.anchorRange)
 
 for (let i = 0; i < 5; i++) {
   previousAnchorX = connectingX + (connectingX - anchor2X)
@@ -45,8 +47,8 @@ for (let i = 0; i < 5; i++) {
   let nextConnectingX = Math.random() * controls.range
   let nextConnectingY = Math.random() * controls.range
 
-  let nextAnchor1X = getRandomNearby(nextConnectingX, 100)
-  let nextAnchor1Y = getRandomNearby(nextConnectingY, 100)
+  let nextAnchor1X = getRandomNearby(nextConnectingX, controls.anchorRange)
+  let nextAnchor1Y = getRandomNearby(nextConnectingY, controls.anchorRange)
 
   createCurve(
     scene,
