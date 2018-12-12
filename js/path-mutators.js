73deff892f6ea67d87dd4a5a5853f2a5354fa850
simonhfrost <simonhfrost@gmail.com>
2018-12-12 14:09:48 +0100

    Create randomizeconnectors

--- js/path-mutators.js
+++ js/path-mutators.js
@@ -13,31 +13,7 @@ function mutateTranslate (path, units) {
 
 // Shift all anchors and connecting positions by a random amount
 function mutateRandomness (path, randomAmount) {
-  const clonedPath = path.clone()
-
-  let prev = null
-  clonedPath.curves.forEach(curve => {
-    if (prev) {
-      // Previous curve must connect to current one
-      curve.v0.x = prev.v3.x
-      curve.v0.y = prev.v3.y
-      curve.v1.x = getComplimentaryPosition(prev.v2.x, prev.v3.x)
-      curve.v1.y = getComplimentaryPosition(prev.v2.y, prev.v3.y)
-    } else {
-      curve.v0.x = getRandomNearby(curve.v0.x, randomAmount)
-      curve.v0.y = getRandomNearby(curve.v0.y, randomAmount)
-      curve.v1.x = getRandomNearby(curve.v1.x, randomAmount)
-      curve.v1.y = getRandomNearby(curve.v1.y, randomAmount)
-    }
-
-    curve.v2.x = getRandomNearby(curve.v2.x, randomAmount)
-    curve.v2.y = getRandomNearby(curve.v2.y, randomAmount)
-    curve.v3.x = getRandomNearby(curve.v3.x, randomAmount)
-    curve.v3.y = getRandomNearby(curve.v3.y, randomAmount)
-
-    prev = curve
-  })
-  return clonedPath
+  return mutateRandomizeAnchors(mutateRandomizeConnectors(path, randomAmount), randomAmount)
 }
 
 function mutateRandomizeAnchors (path, randomAmount) {
@@ -61,4 +37,25 @@ function mutateRandomizeAnchors (path, randomAmount) {
   return clonedPath
 }
 
-export { mutateTranslate, mutateRandomness, mutateRandomizeAnchors }
+function mutateRandomizeConnectors (path, randomAmount) {
+  const clonedPath = path.clone()
+
+  let prev = null
+  clonedPath.curves.forEach(curve => {
+    if (prev) {
+      curve.v0.x = prev.v3.x
+      curve.v0.y = prev.v3.y
+    } else {
+      curve.v0.x = getRandomNearby(curve.v0.x, randomAmount)
+      curve.v0.y = getRandomNearby(curve.v0.y, randomAmount)
+    }
+
+    curve.v3.x = getRandomNearby(curve.v3.x, randomAmount)
+    curve.v3.y = getRandomNearby(curve.v3.y, randomAmount)
+
+    prev = curve
+  })
+  return clonedPath
+}
+
+export { mutateTranslate, mutateRandomness, mutateRandomizeAnchors, mutateRandomizeConnectors }
