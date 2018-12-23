52bd161cac617b398bc1c1d86358d63f7a9e465d
simonhfrost <simonhfrost@gmail.com>
2018-12-23 12:40:51 +0100

    Simplify path creator

--- js/path-creator.js
+++ js/path-creator.js
@@ -1,50 +1,31 @@
 import * as THREE from 'three'
-import { getRandomNearby, getComplimentaryPosition, createVectors } from './util'
+import { getRandomNearby3, getComplimentaryPosition3, createVectors } from './util'
 
 function createPath ({ numPoints, totalRange, anchorDistance }) {
   const curvePath = new THREE.CurvePath()
+  const vectors = createVectors(numPoints, totalRange)
+  let connecting = vectors[0]
+  let previousSecondAnchor = getRandomNearby3(connecting, anchorDistance)
 
-  const points = createVectors(numPoints, totalRange)
-
-  let connectingX = points[0].x
-  let connectingY = points[0].y
-  let connectingZ = points[0].z
-
-  let previousSecondAnchorX = getRandomNearby(connectingX, anchorDistance)
-  let previousSecondAnchorY = getRandomNearby(connectingY, anchorDistance)
-  let previousSecondAnchorZ = getRandomNearby(connectingZ, anchorDistance)
-
-  for (let i = 0; i < points.length - 1; i++) {
-    const firstAnchorX = getComplimentaryPosition(previousSecondAnchorX, connectingX)
-    const firstAnchorY = getComplimentaryPosition(previousSecondAnchorY, connectingY)
-    const firstAnchorZ = getComplimentaryPosition(previousSecondAnchorZ, connectingZ)
-
-    let nextConnectingX = points[i + 1].x
-    let nextConnectingY = points[i + 1].y
-    let nextConnectingZ = points[i + 1].z
-
-    let secondAnchorX = getRandomNearby(nextConnectingX, anchorDistance)
-    let secondAnchorY = getRandomNearby(nextConnectingY, anchorDistance)
-    let secondAnchorZ = getRandomNearby(nextConnectingZ, anchorDistance)
+  for (let i = 0; i < vectors.length - 1; i++) {
+    const firstAnchor = getComplimentaryPosition3(previousSecondAnchor, connecting)
+    let nextConnecting = vectors[i + 1]
+    let secondAnchor = getRandomNearby3(nextConnecting, anchorDistance)
 
     curvePath.add(new THREE.CubicBezierCurve3(
-      new THREE.Vector3(connectingX, connectingY, connectingZ),
-      new THREE.Vector3(firstAnchorX, firstAnchorY, firstAnchorZ),
-      new THREE.Vector3(secondAnchorX, secondAnchorY, secondAnchorZ),
-      new THREE.Vector3(nextConnectingX, nextConnectingY, nextConnectingZ)
+      connecting,
+      firstAnchor,
+      secondAnchor,
+      nextConnecting
     ))
 
-    connectingX = nextConnectingX
-    connectingY = nextConnectingY
-    connectingZ = nextConnectingZ
-    previousSecondAnchorX = secondAnchorX
-    previousSecondAnchorY = secondAnchorY
-    previousSecondAnchorZ = secondAnchorZ
+    connecting = nextConnecting
+    previousSecondAnchor = secondAnchor
   }
 
   return curvePath
 }
 
 export {
-  createPath, getRandomNearby
+  createPath
 }
