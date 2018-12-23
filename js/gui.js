f102082d7bb3d97e01dfe0adb0ce9863da1487d4
simonhfrost <simonhfrost@gmail.com>
2018-12-23 13:36:27 +0100

    Save numPoints to sessionStorage

--- js/gui.js
+++ js/gui.js
@@ -2,21 +2,24 @@ import dat from 'dat.gui'
 
 const Gui = (generate, toggleDebug, exportAnimation, autoRotate) => {
   const gui = new dat.GUI()
+  const sessionStorage = window.sessionStorage
 
   const controls = {
     totalRange: 500,
     anchorDistance: 500,
-    numPoints: 5,
+    numPoints: parseInt(sessionStorage.getItem('numPoints')) || 5,
     toggleDebug: false,
     autoRotate: false,
     generate: generate,
-    example: window.sessionStorage.getItem('selectedExample') || 'simple',
+    example: sessionStorage.getItem('selectedExample') || 'simple',
     exportAnimation: exportAnimation
   }
 
   gui.add(controls, 'totalRange', 0, 1000)
   gui.add(controls, 'anchorDistance', 0, 1000)
-  gui.add(controls, 'numPoints', 0, 20)
+  gui.add(controls, 'numPoints', 0, 20).onChange(value => {
+    sessionStorage.setItem('numPoints', value)
+  })
   gui.add(controls, 'toggleDebug').onChange(toggleDebug)
   gui.add(controls, 'autoRotate').onChange(autoRotate)
   gui.add(controls, 'example', [
@@ -26,7 +29,7 @@ const Gui = (generate, toggleDebug, exportAnimation, autoRotate) => {
     'threeConsistentLines',
     'linesTaperOff'
   ]).onChange(value => {
-    window.sessionStorage.setItem('selectedExample', value)
+    sessionStorage.setItem('selectedExample', value)
   })
   gui.add(controls, 'generate')
   gui.add(controls, 'exportAnimation')
